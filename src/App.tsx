78e713d701928bc63e1b7f8b87affc900bd3a715
shawn <1443584075@qq.com>
2023-07-19 15:30:51 +0800

    feat: 删除多余的登录页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { SafeArea } from "antd-mobile";
-import SignIn from "./route/sign-in/sign-in.component";
 import Navigation from "./route/navigation/navigation.component";
 import NoMatch from "./route/no-match/no-match.component";
 import Authentication from "./route/authentication/authentication.component";
@@ -29,7 +28,6 @@ function App() {
           <Route path="statistics" element={<div>统计</div>} />
           <Route path="user" element={<div>我的</div>} />
         </Route>
-        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/auth" element={<Authentication />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
